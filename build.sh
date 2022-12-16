3259b6aed20eb05f6c172de3acc1f35b48cf34d2
unknown <viczik16@gmail.com>
2022-12-16 12:56:55 +0100

    modified

--- build.sh
+++ build.sh
@@ -3,12 +3,12 @@
 #build the project
 
 echo "Building  Project ...."
-python -m pip install -r requirements.txt
+python3.10.5 -m pip install -r requirements.txt
 
 echo "Make migrations... "
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
+python3.10.5 manage.py makemigrations --noinput
+python3.10.5 manage.py migrate --noinput
 
 echo "Collect static ..."
-python manage.py collectstatic --noinput --clear
+python3.10.5 manage.py collectstatic --noinput --clear
 
