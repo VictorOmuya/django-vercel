1256e1fd7cab813f83480d504a8328d15831d6b0
Victor Omuya <59045773+victoromuya@users.noreply.github.com>
2023-02-18 07:03:49 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,12 +3,12 @@
 #build the project
 
 echo "Building  Project ...."
-python3.9 -m pip install -r requirements.txt
+python -m pip install -r requirements.txt
 
 echo "Make migrations... "
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
 
 echo "Collect static ..."
-python3.9 manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput --clear
 
